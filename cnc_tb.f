b8a0850e6cc34e77fbfb2696079b32517211e92e
vkorehov <vladlv@inbox.lv>
2014-05-08 20:31:44 +0100

    initial modelsim

---
+++ cnc_tb.f
@@ -0,0 +1,20 @@
++licq_all+
++access+r
+
+./pci_lc_i.v
+./pcim_lc_33_5_s.v
+
+./cfg_ping.v
+./ping.v
+./pcim_top.v
+./busrecord.v
+./dumb_arbiter.v
+./dumb_targ32.v
+./stimulus.v
+./ping_tb.v
+
+$XILINX/verilog/src/glbl.v
+
++libext+.vmd+.v
+-y $XILINX/verilog/src/unisims
+-y $XILINX/verilog/src/simprims
